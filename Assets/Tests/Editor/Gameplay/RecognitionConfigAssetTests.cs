efbf2ca61a0a7eb802b6d8d854b7abfb60bb8c0f
Ian Clyde E. Tejada <tejadaianc@gmail.com>
2026-06-03 01:52:04 +0800

    chore: configure mobile stroke thresholds

---
+++ Assets/Tests/Editor/Gameplay/RecognitionConfigAssetTests.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using NUnit.Framework;
+
+namespace Salinlahi.Tests.Editor.Gameplay
+{
+    [TestFixture]
+    public class RecognitionConfigAssetTests
+    {
+        [Test]
+        public void DefaultRecognitionConfig_SerializesMobileStrokeThresholds()
+        {
+            string yaml = File.ReadAllText("Assets/ScriptableObjects/RecognitionConfig_Default.asset");
+
+            StringAssert.Contains("rawSampleMinDistancePixels: 2", yaml);
+            StringAssert.Contains("visualSampleSpacingPixels: 8", yaml);
+            StringAssert.Contains("maxVisualSamplesPerSegment: 24", yaml);
+            StringAssert.Contains("minimumStrokePathLengthPixels: 40", yaml);
+            StringAssert.Contains("minimumStrokeBoundsPixels: 12", yaml);
+        }
+    }
+}
